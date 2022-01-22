5cb890d55eb02dc318105fe37577978355669408
idoodi <idoodi@ya.ru>
2022-01-22 23:20:49 +0300

    1

--- src/addit/3_useRef.js
+++ src/addit/3_useRef.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 
 function App() {
-    
+
     // const [numState, setNumState] = useState(1)
     const [valueState, setValueState] = useState(1)
     const countRef = useRef(1)
@@ -9,36 +9,24 @@ function App() {
     const prevRef = useRef('')
 
 
-useEffect(()=>{
-   
-    //получим бесконечное выполнение, само прибавление вызывает рендер, 
-    //  setNumState((prev)=>prev+1)
 
-    // работает 'между' рендерами, само прибавление не вызывает рендер
-    console.log("количество рендеров",countRef.current++)
 
-    console.log("предыдущее состояние", prevRef.current)
-    
-    console.log("текущее состояние", inputRef.current.value)
-    
-})
-
-useEffect(()=>{prevRef.current = valueState}, [valueState])
+    useEffect(() => { prevRef.current = valueState }, [valueState])
 
 
     return (
         <>
 
-
-{/* 
+            {/* 
 привязали inputRef к инпуту 
 изменяем стейт(для вызова useEffect)
 */}
-<input ref={inputRef} type='text' onChange={e => setValueState(e.target.value)} value={valueState}/>
-<button onClick={()=> {inputRef.current.focus()}}>фокус</button>
-<h1>количество рендеров: {countRef.current++}</h1>
-<h1>предыдущее состояние: {prevRef.current}</h1>
-<h1>текущее состояние: {valueState}</h1>
+            <input ref={inputRef} type='text' onChange={e => setValueState(e.target.value)} value={valueState} />
+            <button onClick={() => { inputRef.current.focus() }}>фокус</button>
+            <h1>количество рендеров: {countRef.current++}</h1>
+            <h1>предыдущее состояние: {prevRef.current}</h1>
+            <h1>текущее состояние: {valueState}</h1>
+
         </>
     )
 

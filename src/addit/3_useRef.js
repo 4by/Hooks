7a6474265ccea5cb3bba6e7ba4bd549e1c9047e4
idoodi <idoodi@ya.ru>
2022-02-07 21:40:46 +0300

    1

--- src/addit/3_useRef.js
+++ src/addit/3_useRef.js
@@ -2,26 +2,25 @@ import { useState, useEffect, useRef } from "react";
 
 function App() {
 
-    const [valueState, setValueState] = useState(1)
-    const numRef = useRef(1)
-    const textRef = useRef("null")
+    const [textState, setTextState] = useState('')
+    const focusRef = useRef("null")
     const prevTextRef = useRef('')
 
 
-    useEffect(() => { prevTextRef.current = valueState }, [valueState])
+    useEffect(() => { prevTextRef.current = textState }, [textState])
 
 
     return (
         <>
 
-            {/* привязали textRef к инпуту; изменяем стейт(для вызова useEffect)*/}
-            <input ref={textRef} type='text' value={valueState} onChange={e => setValueState(e.target.value)} />
-            <button onClick={() => { textRef.current.focus() }}>фокус</button>
+            {/*изменяем стейт(для вызова useEffect)*/}
+            <input ref={focusRef} type='text' value={textState} onChange={e => setTextState(e.target.value)} />
+            {/*привязали focusRef к инпуту; */}
+            <button onClick={() => { focusRef.current.focus() }}>фокус</button>
             {/* нельзя использовать useState так как он делает ре-рендер */}
-            <h1>количество рендеров: {numRef.current++}</h1>
             <h1>предыдущее состояние: {prevTextRef.current}</h1>
             {/* нельзя использовать useRef так как он назначается 'между' рендерами и в 'живом' режиме его не увидеть */}
-            <h1>текущее состояние: {valueState}</h1>
+            <h1>текущее состояние: {textState}</h1>
 
         </>
     )
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 
- import App from './basic/1_useState';
+//  import App from './basic/1_useState';
 // import App from './basic/2_useEffect';
 //  import App from './basic/3_useContext';
 // import App from './addit/1_useReducer';
 //  import App from './addit/2_use(Memo, Callback)';
-// import App from './addit/3_useRef';
+import App from './addit/3_useRef';
 // import App from './customHook';
 
 ReactDOM.render(
@@ -16,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
